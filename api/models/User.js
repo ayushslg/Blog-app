ded40fc43b7690137bea00f5dbb53e5fd5639983
ayush <ayushtiwari.slg01@gmail.com>
2024-01-07 16:43:23 +0530

    Added tags feature in frontend

--- api/models/User.js
+++ api/models/User.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
             required:true,
         },
         profilePic:{
-            typr:String,
+            type:String,
             default: "",
         }
     },
